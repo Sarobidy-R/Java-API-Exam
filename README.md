662a901c0d4bfbb6e809e5069adb01087f723005
Sarobidy-R <rasendraminosarobidy@gmail.com>
2025-07-09 15:15:46 +0300

    modification basique du fichier principale (App.java)

--- README.md
+++ README.md
@@ -15,6 +15,9 @@ Pour exécuter ce projet localement à l'aide de Docker Compose, suivez les éta
     docker compose up --build -d # lancement en arrière-plan
 ```
 
+
+#### L'application sera accessible via l'url : [localhost:8080](http://localhost:8080).
+
 ### Arrêter les services
 
 Pour arrêter les services, utilisez :
--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Holla depuis le serveur Java API ! 🚀";
+            exchange.getResponseHeaders().set("Content-Type", "text/plain; charset=UTF-8");
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        });
+
+        server.setExecutor(null);
+        System.out.println("✅ Serveur HTTP démarré sur le port " + port + " 🎉");
+        System.out.println("🌐 Accédez à l'API via http://localhost:" + port);
+        server.start();
     }
 }
